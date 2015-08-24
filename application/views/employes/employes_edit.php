7430850d10d8e4df1016eba0c76faaff64d33115
Nadir Malik <Nadir Malik@192.168.1.9>
2015-08-24 11:15:28 -0700

    first commit

--- application/views/employes/employes_edit.php
+++ application/views/employes/employes_edit.php
@@ -1,48 +1,60 @@
-
+<div class="alert alert-danger">
  <?php echo validation_errors(); ?>
+</div>
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+<?php echo form_open('employes/create') ?>
+<div class="form-group">
+    <label for="name">Emp Id</label>
+    <input type="text" class="form-control" name="empid" id="empid "value="<?php echo $emp[0]->id;?>"
+       placeholder="Enter id">
+</div>
+<div class="form-group">
+    <label for="name">Emp Cnic</label>
+    <input type="text" class="form-control" name="empcnc" id="empcnc"value="<?php echo $emp[0]->Emp_cnic;?>"
+       placeholder="Enter cnic">
+</div>
+        <div class="form-group">
+    <label for="name">Emp Name</label>
+    <input type="text" class="form-control" name="empname" id="empname"value="<?php echo $emp[0]->Emp_name;?>"
+       placeholder="Enter Name">
+</div>
 
-<?php echo form_open('employes/save_emp') ?>
-<table width="332" height="203" border="1" align="center">
-  <tr>
-    <td height="28" colspan="2"><div align="center">Employes Form</div></td>
-    </tr>
-  <tr>
-    <td width="102" height="29">Emp_name</td>
-    <td width="199"><label for="nm"></label>
-        <input type="text" name="nm" id="nm" value="<?php echo $emp[0]->Emp_name;?>" /></td>
-  </tr>
-  <tr>
-    <td height="33">Emp_father_name</td>
-    <td><label for="fm"></label>
-      <input type="text" name="fm" id="fm" value="<?php echo $emp[0]->Emp_father_name;?>" /></td>
-  </tr>
-  <tr>
-    <td height="34"><u>Emp cnic</u></td>
-    <td><label for="cnc"></label>
-      <input type="text" name="cnc" id="cnc" value="<?php echo $emp[0]->Emp_cnic;?>" /></td>
-  </tr>
-  <tr>
-    <td height="32">Emp_scale</td>
-    <td><label for="scale"></label>
-      <input type="text" name="scale" id="scale" value="<?php echo $emp[0]->Emp_scale;?>" /></td>
-  </tr>
-  <tr>
-    <td>Emp_post</td>
-    <td><label for="post"></label>
-      <input type="text" name="post" id="post" value="<?php echo $emp[0]->Emp_post;?>" /></td>
-  </tr>
-  <tr>
-    <td>Emp_address</td>
-    <td><label for="add"></label>
-      <textarea name="add" id="add" value="<?php echo $emp[0]->Emp_address;?>" cols="45" rows="5" ></textarea></td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
+  <div class="form-group">
+    <label for="name">Emp Father Name</label>
+    <input type="text" class="form-control" name="empfather" id="empfather" value="<?php $emp$std[0]->Emp_father_name;?>" 
+       placeholder="Enter Father Name">
+</div>
+    <div class="form-group">
+    <label for="name">Emp Scale</label>
+    <input type="text" class="form-control" name="empscale" id="empscale"value="<?php echo $emp[0]->Emp_scale;?>"
+    placeholder="Enter Scale">
+    </div>
+        <div class="form-group">
+    <label for="name">Emp Post</label>
+    <input type="text" class="form-control" name="emppost" id="emppost"value="<?php echo $emp[0]->Emp_post;?>" 
+    placeholder="Entir post">
+    </div>
+        <div class="form-group">
+    <label for="name">Emp Address</label>
+    <input type="text" class="form-control" name="empadd" id="empadd"value="<?php echo $emp[0]->Emp_address;?>" 
+    placeholder="Enter Address">
+    </div>
+    
+    
+    
+   
+       
+   <td>&nbsp;</td>
     <td><input type="submit" name="sbtn" id="sbtn" value="Submit" /></td>
   </tr>
 </table>
 
-<input type="hidden" name="id" id="id" value="<?php echo $emp[0]->id;?>" />
+
 </form>
+</div>
+<div class="col-md-3"></div>
+</div>
 </body>
 </html>
