80ede58f68c59f53f77b0f36c493b692d8744084
Nadir Malik <Nadir Malik@192.168.1.9>
2015-08-24 11:16:39 -0700

    first commit

--- application/views/employes/employes_view.php
+++ application/views/employes/employes_view.php
@@ -6,39 +6,47 @@
     <div class="col-md-6">
 <?php echo form_open('employes/create') ?>
 <div class="form-group">
-    <label for="name">Employee Name</label>
+    <label for="name">Emp Id</label>
+    <input type="text" class="form-control" name="empid" id="empid" 
+       placeholder="Enter id">
+</div>
+<div class="form-group">
+    <label for="name">Emp Cnic</label>
+    <input type="text" class="form-control" name="empcnc" id="empcnc" 
+       placeholder="Enter cnic">
+</div>
+        <div class="form-group">
+    <label for="name">Emp Name</label>
     <input type="text" class="form-control" name="empname" id="empname" 
        placeholder="Enter Name">
 </div>
-<div class="form-group">
-    <label for="name">Father Name</label>
-    <input type="text" class="form-control" name="fathername" id="fathername" 
-       placeholder="Enter Name">
+
+  <div class="form-group">
+    <label for="name">Emp Father Name</label>
+    <input type="text" class="form-control" name="empfather" id="empfather" 
+       placeholder="Enter Father Name">
 </div>
-<table width="332" height="203" border="1" align="center" >
-  
-  <tr>
-    <td height="34"><u>Emp cnic</u></td>
-    <td><label for="cnc"></label>
-      <input type="text" name="cnc" id="cnc" /></td>
-  </tr>
-  <tr>
-    <td height="32">Emp_scale</td>
-    <td><label for="scale"></label>
-      <input type="text" name="scale" id="scale" /></td>
-  </tr>
-  <tr>
-    <td>Emp_post</td>
-    <td><label for="post"></label>
-      <input type="text" name="post" id="post" /></td>
-  </tr>
-  <tr>
-    <td>Emp_address</td>
-    <td><label for="add"></label>
-      <textarea name="add" id="add" cols="45" rows="5"></textarea></td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
+    <div class="form-group">
+    <label for="name">Emp Scale</label>
+    <input type="text" class="form-control" name="empscale" id="empscale" 
+    placeholder="Enter Scale">
+    </div>
+        <div class="form-group">
+    <label for="name">Emp Post</label>
+    <input type="text" class="form-control" name="emppost" id="emppost" 
+    placeholder="Entir post">
+    </div>
+        <div class="form-group">
+    <label for="name">Emp Address</label>
+    <input type="text" class="form-control" name="empadd" id="empadd" 
+    placeholder="Enter Address">
+    </div>
+    
+    
+    
+   
+       
+   <td>&nbsp;</td>
     <td><input type="submit" name="sbtn" id="sbtn" value="Submit" /></td>
   </tr>
 </table>
